a3e5bee7c06f0356f49dd0d00cd250d6687d6247
David Flegal <flegal.david@gmail.com>
2016-03-01 15:22:45 -0800

    added minor changes

--- day2/src/series.py
+++ day2/src/series.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def series(i, a, b):
+def sum_series(i, a=0, b=1):
     """returns the n-th value of the requested sequence fibonacci or lucas"""
     if i == 1:
         return a
@@ -21,9 +21,26 @@ def series(i, a, b):
 # return the 'n' value of the fibonacci sequence
 # 0, 1, 1, 2, 3, 5, 8, 13, 21 ....
 def fibonacci(n):
-    return series(n, 0, 1)
+    return sum_series(n, 0, 1)
 
 # return the 'n' value of the lucas sequence
 # 2, 1, 3, 4, 7, 11, 18, 29 ....
 def lucas(n):
-    return series(n, 2, 1)
+    return sum_series(n, 2, 1)
+
+if __name__ == "__main__":
+    """This module defines functions that implement mathematical series.
+    ...
+
+    fibonacci(n):
+
+        Returns the nth value in the fibonacci series
+
+    >>> fibonacci(2)
+    1
+
+    lucas(n):
+        Returns the nth value in the lucas sum_series
+
+    sum_series(n, a, b):
+        returns the nth value of a custom series with a and b being starting points(0,1) are default"""
